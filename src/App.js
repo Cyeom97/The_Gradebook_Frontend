672352f248d70b36b4cec0c12f653604a42c8878
rebecca <fulkerson.rebecca@gmail.com>
2022-11-21 14:58:42 -0500

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Students from './pages/Students';
+import StudentDetails from './pages/StudentDetails';
+import Courses from './pages/Courses';
+import CourseDetails from './pages/CourseDetails';
+import AddStudentForm from './components/AddStudentForm';
+import AddCourseForm from './components/AddCourseForm';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/students" element={ <Students/> } />
+          <Route path="/studentdetails" element={ <StudentDetails/> } />
+          <Route path="/courses" element={ <Courses/> } />
+          <Route path="/coursedetails" element={ <CourseDetails/> } />
+          <Route path="/AddStudent" element={ <AddStudentForm/> } />
+          <Route path="/AddCourse" element={ <AddCourseForm/> } />
+        </Routes>
+      </main>
     </div>
   );
 }
