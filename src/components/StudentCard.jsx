612a99f0cb1a51945da7e86e8134e0121e5d4f1c
Marissa Dilig <hello.maripd@gmail.com>
2022-11-21 14:00:20 -0600

    added students and student card page

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,8 +1,21 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
+const StudentCard = (props) => {
+    const navigate = useNavigate()
 
-const StudentCard = () => {
+    const cardHandleClick = () => {
+        navigate(`/____/${props.id}`)
+    }
 
-    return
+    return (
+        <ul  onClick={cardHandleClick} className="studentcard-container">
+            <li className="studentcard">
+                <h3>{props.studentName}</h3>
+                <p>{props.overallGpa}</p>
+            </li>
+        </ul>
+    )
 }
 
 export default StudentCard
