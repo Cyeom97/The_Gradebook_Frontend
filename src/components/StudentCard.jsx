47eaf2ee33f7f76a94a6949a0e36979c9e2cf0a8
rebecca <fulkerson.rebecca@gmail.com>
2022-11-22 01:35:11 -0500

    finished readme

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -10,15 +10,15 @@ const StudentCard = (props) => {
     }
 
    
-    return (
-        <ul  className="studentcard-container">
-            {/* <li onClick={cardHandleClick} className="studentcard">
-                <h3 className='student-name'>{props.name}</h3>
-                <p className='student-gpa'>GPA: {props.gpa}</p>
-            </li> */}
-            <div>Hello</div>
-        </ul>
-    )
+    return 
+        // <ul  className="studentcard-container">
+        //     {/* <li onClick={cardHandleClick} className="studentcard">
+        //         <h3 className='student-name'>{props.name}</h3>
+        //         <p className='student-gpa'>GPA: {props.gpa}</p>
+        //     </li> */}
+        //     <div>Hello</div>
+        // </ul>
+    
 }
 
 export default StudentCard
