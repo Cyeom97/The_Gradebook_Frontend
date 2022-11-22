404c817dd0d4924183ea74a5b2f24d8765bbae82
rebecca <fulkerson.rebecca@gmail.com>
2022-11-22 11:35:40 -0500

    deleted one more thing

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
           <Route path="/studentdetails" element={<StudentDetails />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/:courseId" element={<CourseDetails />} />
-          <Route path="/AddStudent" element={<AddStudentForm />} />
           <Route path="/AddCourse" element={<AddCourseForm />} />
           <Route path="/courses/create" element={<AddCourseForm />} />
           <Route path="/studentcourses" element={<StudentCourse />} />
